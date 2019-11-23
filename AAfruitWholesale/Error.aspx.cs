7a1f7bc786b53fff83d783e2dd847cca563f38d6
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2019-11-23 23:33:43 +0400

    remove about us and contacty us add error page

--- AAfruitWholesale/Error.aspx.cs
+++ AAfruitWholesale/Error.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace AAfruitWholesale
 {
-    public partial class Contact : Page
+    public partial class Error : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
